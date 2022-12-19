de27f29e8eaf577c2f9127e428378286d7de4886
ZamanS94 <106835600+ZamanS94@users.noreply.github.com>
2022-12-19 21:26:37 +0100

    Update urls.py

--- Deployment/learning_templates/basic_app/urls.py
+++ Deployment/learning_templates/basic_app/urls.py
@@ -8,5 +8,5 @@ app_name = 'basic_app'
 urlpatterns = [
     re_path('relative/',views.relative,name='relative'),
     path('other/',views.other,name='other'),
-    path('base/',views.base,name='other'),
+    path('base/',views.other,name='other'),
 ]
